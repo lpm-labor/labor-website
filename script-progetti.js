a8c48d20894b0905a61849000024c62a3f0c7acd
lpm-labor <albertostievanin@libero.it>
2025-12-10 12:56:42 +0100

    Update script-progetti.js

--- script-progetti.js
+++ script-progetti.js
@@ -37,7 +37,7 @@ const labelMap = {
     materiali: "materiali",
     dimensioni: "dimensioni",
     quantità: "quantità",
-    tecnica_lavorazione: "lavorazioni",     // ⭐ aggiornato
+    tecnica_lavorazione: "processo di lavorazione",     // ⭐ aggiornato
     energia_produzione: "energia utilizzata",
     link: "link"
 };
